df572da67c23a2fd735bc342302049e0c536d01a
Recelis <jackymlui@live.com>
2022-04-07 07:37:43 +1000

    blinking program test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
 
+#define LED D0
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("blinking");
+  digitalWrite(LED,LOW);
+  delay(1000);
+  digitalWrite(LED, HIGH);
+  delay(1000);
 }
